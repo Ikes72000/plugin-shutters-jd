e2e6bcf8a63941c6e3a829ae94cdb0d01bf26a08
Unknown <seki72000@gmail.com>
2019-06-18 22:27:40 +0200

    Création plugin

---
+++ core/class/shutters.class.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+ * This file is part of the NextDom software (https://github.com/NextDom or http://nextdom.github.io).
+ * Copyright (c) 2018 NextDom.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* * ***************************Includes********************************* */
+require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+require_once 'shuttersCmd.class.php';
+
+class shutters extends eqLogic
+{
+    /*     * *************************Attributs****************************** */
+
+
+    /*     * ***********************Methode static*************************** */
+
+    /*
+     * Fonction exécutée automatiquement toutes les minutes par Jeedom
+      public static function cron() {
+
+      }
+     */
+
+
+    /*
+     * Fonction exécutée automatiquement toutes les heures par Jeedom
+      public static function cronHourly() {
+
+      }
+     */
+
+    /*
+     * Fonction exécutée automatiquement tous les jours par Jeedom
+      public static function cronDaily() {
+
+      }
+     */
+
+
+    /*     * *********************Méthodes d'instance************************* */
+
+    public function preInsert()
+    {
+
+    }
+
+    public function postInsert()
+    {
+
+    }
+
+    public function preSave()
+    {
+
+    }
+
+    public function postSave()
+    {
+
+    }
+
+    public function preUpdate()
+    {
+
+    }
+
+    public function postUpdate()
+    {
+
+    }
+
+    public function preRemove()
+    {
+
+    }
+
+    public function postRemove()
+    {
+
+    }
+
+}
